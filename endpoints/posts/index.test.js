a03762ee0ae9ebe91021bfada4c08c978a6f17e9
manci <javiercaliescali@gmail.com>
2021-01-18 14:50:14 -0500

    crear segundo test de integracion

--- endpoints/posts/index.test.js
+++ endpoints/posts/index.test.js
@@ -13,7 +13,17 @@ describe('Server', () => {
           console.log(response);
         expect(response.statusCode).toEqual(201)
         expect(response.body.userId).toEqual(5)
-        expect(response.body).tohaveProperty('id')
+        expect(response.body).toHaveProperty('id')
+      })
+
+      it('does not create a new post', async () => {
+        const response = await request(app)
+          .post('/')
+          .send({ userId: 100 })
+          .set('user_id', 1)
+          .set('Content-Type', 'application/json')
+          console.log(response);
+        expect(response.statusCode).toEqual(400)
       })
     })
   })
