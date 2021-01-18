ab720578b3403ceaf25e4433cc2f274e92d68a0c
manci <javiercaliescali@gmail.com>
2021-01-17 20:38:57 -0500

    tests pasan

--- endpoints/posts/index.js
+++ endpoints/posts/index.js
@@ -1,8 +1,8 @@
 module.exports = ({ axios }) => ({
   post: async (req, res) => {
     await axios.get('https://jsonplaceholder.typicode.com/users')
-    await axios.post('https://jsonplaceholder.typicode.com/posts', req.body)
-    res.status(201).send({ id: 1000 })
+    const { data } = await axios.post('https://jsonplaceholder.typicode.com/posts', req.body)
+    res.status(201).send(data)
   } 
 })
 
